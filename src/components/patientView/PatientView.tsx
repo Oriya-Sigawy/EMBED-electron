a4940aa3741d731c4e0e783db9dcbc60836bc4f7
yuvili <yuvalsh97@gmail.com>
2024-09-21 12:58:30 +0300

    feat(TAS-63): Go back button to Home from Patient View

--- src/components/patientView/PatientView.tsx
+++ src/components/patientView/PatientView.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const getPatientDetails = (patientId: string) => {
-  return `Patient ID: ${patientId}`;
-};
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button as MuiButton } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
 
 export default function PatientView() {
   const { patientId } = useParams();
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate(`/`);
+  };
+
+  const getPatientDetails = (patientId: string) => {
+    return `Patient ID: ${patientId}`;
+  };
 
   const patientDetails = getPatientDetails(patientId);
 
   return (
     <div>
+      <MuiButton size="medium" onClick={goToHome}>
+        <ArrowBack />
+        Back
+      </MuiButton>
       <h2>Patient {patientId.split('_')[1]}</h2>
       <div>{patientDetails}</div>
     </div>
