6f1806443b8970ebd40a1efe960103ecc0d6316a
Oriya-Sigawy <Oriyas.07@gmail.com>
2025-02-13 12:55:07 +0200

    added some documentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;   //ensuring that container will be treated as HTML element
 const root = createRoot(container);
 root.render(<App />);
 
 declare global {
   interface Window {
     DDSM_AGENT?: {
-      channels: any;
-      send: (channel: string, data?: any) => Promise<any>;
-      listen: (channel: string, func: (event: any, ...args: any[]) => void) => void;
-      removeListener: (channel: string) => void;
-      closeWindow: () => void;
+      channels: any;                                            //stores some kind of channel-related data.
+      send: (channel: string, data?: any) => Promise<any>;      //ends data to a specific channel and returns a promise.
+      listen: (channel: string, func: (event: any, ...args: any[]) => void) => void;    //Allows listening to messages from a specific channel.
+      removeListener: (channel: string) => void;                //Removes a listener from a specific channel.
+      closeWindow: () => void;                                  //closes the current application window.
     };
   }
 }
